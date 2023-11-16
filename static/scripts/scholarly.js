6ba15c67597b9fc7bb3b5c0bdca54369173543ef
hankypoo7 <145811689+hankypoo7@users.noreply.github.com>
2023-11-16 12:35:55 -0600

    Update scholarly.js

--- static/scripts/scholarly.js
+++ static/scripts/scholarly.js
@@ -32,7 +32,6 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
 
         doc.head.appendChild(link);
         doc.body.appendChild(iframe)
-        location.replace("https://www.nasa.gov/")
     }
 }
 
